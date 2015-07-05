1016615ebd336d841029227ba777f74849b16b70
stefvanschie <stef@van-schie.eu>
2015-07-05 19:39:11 +0200

    Update Scoreboardsetup.java

--- src/me.stefvanschie/Scoreboardsetup.java
+++ src/me.stefvanschie/Scoreboardsetup.java
@@ -1,6 +1,5 @@
 package me.stefvanschie;
 
-import org.bukkit.ChatColor;
 import org.bukkit.scoreboard.DisplaySlot;
 
 public class Scoreboardsetup
@@ -10,6 +9,7 @@ public class Scoreboardsetup
         BuildingGame.main.scoreboard = BuildingGame.main.manager.getNewScoreboard();
         BuildingGame.main.objective = BuildingGame.main.scoreboard.registerNewObjective("votes", "dummy");
         BuildingGame.main.objective.setDisplaySlot(DisplaySlot.SIDEBAR);
-        BuildingGame.main.objective.setDisplayName(ChatColor.YELLOW + "Points");
+        BuildingGame.main.objective.setDisplayName(BuildingGame.main.messages.getString("global.scoreboardHeader")
+        		.replaceAll("&", "§"));
 	}
 }
