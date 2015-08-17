c6dbbc522b6a875a4b552bf0162df5527163bee3
stefvanschie <stef@van-schie.eu>
2015-08-17 17:14:12 +0200

    Update Scoreboardsetup.java

--- src/me.stefvanschie/Scoreboardsetup.java
+++ src/me.stefvanschie/Scoreboardsetup.java
@@ -7,8 +7,11 @@ public class Scoreboardsetup
 	public static void setup()
 	{
         BuildingGame.main.scoreboard = BuildingGame.main.manager.getNewScoreboard();
+        
         BuildingGame.main.objective = BuildingGame.main.scoreboard.registerNewObjective("votes", "dummy");
         BuildingGame.main.objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+        
+        FileCheck.check("global.scoreboardHeader", "&ePoints", BuildingGame.main.messages, BuildingGame.main);
         BuildingGame.main.objective.setDisplayName(BuildingGame.main.messages.getString("global.scoreboardHeader")
         		.replaceAll("&", "§"));
 	}
