747b714f054e64012f381358e5dff6005bfeedec
stefvanschie <stef@van-schie.eu>
2015-08-17 17:15:09 +0200

    Update Setminplayers.java

--- src/me.stefvanschie/Setminplayers.java
+++ src/me.stefvanschie/Setminplayers.java
@@ -7,7 +7,8 @@ public class Setminplayers
 	public static void setminplayers(Player player, String arena, int minplayers)
 	{
 		BuildingGame.main.arenas.set(arena + ".minplayers", minplayers);
-		player.sendMessage(BuildingGame.main.messages.getString("global.prefix").replaceAll("&", "§") + BuildingGame.main.messages.getString("setminplayers.succes")
+		BuildingGame.main.saveYamls();
+		player.sendMessage(BuildingGame.main.messages.getString("global.prefix").replaceAll("&", "§") + BuildingGame.main.messages.getString("setMinPlayers.succes")
 				.replaceAll("&", "§"));
 	}
 }
