dfdea2d31f95058d4df90b8fead8eb48de83e474
stefvanschie <stef@van-schie.eu>
2015-08-17 17:16:40 +0200

    Update Vote.java

--- src/me.stefvanschie/Vote.java
+++ src/me.stefvanschie/Vote.java
@@ -10,16 +10,29 @@ public class Vote {
 		if (player.hasPermission("bg.vote")) {
 			if (BuildingGame.main.players.containsKey(player)) {
 				if (points >= 1 && points <= 10) {
-					Player pl = BuildingGame.main.playernumbers.get(BuildingGame.main.place);
-					BuildingGame.main.votes.put(pl, points + BuildingGame.main.votes.get(pl));
-					//scoreboard
-					BuildingGame.main.score = BuildingGame.main.objective.getScore(BuildingGame.main.playernumbers.get(BuildingGame.main.place));
-					BuildingGame.main.score.setScore(BuildingGame.main.votes.get(BuildingGame.main.playernumbers.get(BuildingGame.main.place)));
-					//message
-					player.sendMessage(BuildingGame.main.messages.getString("global.prefix").replaceAll("&", "§") + BuildingGame.main.messages.getString("vote.message")
-							.replace("%playerplot%", BuildingGame.main.playernumbers.get(BuildingGame.main.place) + "")
-							.replace("%points%", points + "")
-							.replaceAll("&", "§"));
+					if (BuildingGame.main.playernumbers.get(BuildingGame.main.place) != player) {
+						Player pl = BuildingGame.main.playernumbers.get(BuildingGame.main.place);
+						if (BuildingGame.main.playervotes.containsKey(player)) {
+							int previousPoints = BuildingGame.main.playervotes.get(player);
+							BuildingGame.main.votes.put(pl, points + BuildingGame.main.votes.get(pl) - previousPoints);
+							BuildingGame.main.playervotes.put(player, points);
+						} else {
+							BuildingGame.main.votes.put(pl, points + BuildingGame.main.votes.get(pl));
+							BuildingGame.main.playervotes.put(player, points);
+						}
+						//scoreboard
+						BuildingGame.main.score = BuildingGame.main.objective.getScore(BuildingGame.main.playernumbers.get(BuildingGame.main.place));
+						BuildingGame.main.score.setScore(BuildingGame.main.votes.get(BuildingGame.main.playernumbers.get(BuildingGame.main.place)));
+						//message
+						player.sendMessage(BuildingGame.main.messages.getString("global.prefix").replaceAll("&", "§") + BuildingGame.main.messages.getString("vote.message")
+								.replace("%playerplot%", BuildingGame.main.playernumbers.get(BuildingGame.main.place).getName() + "")
+								.replace("%points%", points + "")
+								.replaceAll("&", "§"));
+					} else if (BuildingGame.main.playernumbers.get(BuildingGame.main.place) == player) {
+						player.sendMessage(ChatColor.RED + "You can't vote on your own plot");
+					} else {
+						player.sendMessage(ChatColor.RED + "An unexpected error occured. Error: bg.Vote.vote.playernumbers.get");
+					}
 				} else if (!(points >= 1 && points <= 10)) {
 					player.sendMessage(ChatColor.RED + "Choose a number between 1 and 10");
 				} else {
