b68973333c93cf9ade9063ce4ad352d11dbeb554
stefvanschie <stef@van-schie.eu>
2015-09-06 17:09:06 +0200

    BuildTimer.java

---
+++ me/stefvanschie/buildinggame/timers/BuildTimer.java
@@ -0,0 +1,63 @@
+package me.stefvanschie.buildinggame.timers;
+
+import me.confuser.barapi.BarAPI;
+import me.stefvanschie.buildinggame.Main;
+import me.stefvanschie.buildinggame.managers.files.SettingsManager;
+import me.stefvanschie.buildinggame.managers.messages.MessageManager;
+import me.stefvanschie.buildinggame.managers.softdependencies.SDBarApi;
+import me.stefvanschie.buildinggame.utils.Arena;
+import me.stefvanschie.buildinggame.utils.GameState;
+import me.stefvanschie.buildinggame.utils.Plot;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class BuildTimer extends BukkitRunnable {
+
+	private int seconds;
+	private int originalSeconds;
+	private Arena arena;
+	
+	public BuildTimer(int seconds, Arena arena) {
+		this.seconds = seconds;
+		originalSeconds = seconds;
+		this.arena = arena;
+	}
+	
+	@Override
+	public void run() {
+		YamlConfiguration messages = SettingsManager.getInstance().getMessages();
+		
+		if (seconds <= 0) {
+			//voten
+			for (Plot plot : arena.getPlots()) {
+				if (plot.getPlayerData() != null) {
+					Player player = plot.getPlayerData().getPlayer();
+					if (SDBarApi.getInstance().isEnabled()) {
+						if (BarAPI.hasBar(player)) {
+							BarAPI.removeBar(player);
+						}
+					}
+				}
+			}
+			arena.getVoteTimer().runTaskTimer(Main.getInstance(), 20L, 20L);
+			arena.setState(GameState.VOTING);
+			this.cancel();
+		} else if ((seconds != originalSeconds && seconds % 60 == 0) || seconds == 30 || seconds == 15 || (seconds <= 10 && seconds >= 1)) {
+			for (Plot plot : arena.getPlots()) {
+				if (plot.getPlayerData() != null) {
+					Player player = plot.getPlayerData().getPlayer();
+					MessageManager.getInstance().send(player, messages.getString("buildingCountdown.message")
+							.replace("%seconds%", seconds + "")
+							.replaceAll("&", "§"));
+				}
+			}
+		}
+		seconds--;
+	}
+	
+	public int getSeconds() {
+		return seconds;
+	}
+}
