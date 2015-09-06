1b3dbda563ca5400c1b2bb0609831a77615f1d5c
stefvanschie <stef@van-schie.eu>
2015-09-06 16:55:06 +0200

    First.java

---
+++ me/stefvanschie/buildinggame/commands/subcommands/settings/money/First.java
@@ -0,0 +1,60 @@
+package me.stefvanschie.buildinggame.commands.subcommands.settings.money;
+
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+
+import me.stefvanschie.buildinggame.commands.commandutils.CommandResult;
+import me.stefvanschie.buildinggame.commands.commandutils.SubCommand;
+import me.stefvanschie.buildinggame.managers.files.SettingsManager;
+import me.stefvanschie.buildinggame.managers.messages.MessageManager;
+
+public class First extends SubCommand {
+
+	@Override
+	public CommandResult onCommand(Player player, String[] args) {
+		YamlConfiguration config = SettingsManager.getInstance().getConfig();
+		
+		if (args.length == 0) {
+			MessageManager.getInstance().send(player, ChatColor.RED + "Please specify the money");
+			return CommandResult.ARGUMENTEXCEPTION;
+		}
+		
+		try {
+			Double.parseDouble(args[0]);
+		} catch (NumberFormatException nfe) {
+			MessageManager.getInstance().send(player, ChatColor.RED + "That's not a valid number");
+			return CommandResult.ERROR;
+		}
+		
+		double money = Double.parseDouble(args[0]);
+		
+		config.set("money.first", money);
+		SettingsManager.getInstance().save();
+		
+		MessageManager.getInstance().send(player, ChatColor.GREEN + "Money setting set to " + money);
+		
+		return CommandResult.SUCCES;
+	}
+
+	@Override
+	public String getName() {
+		return "first";
+	}
+
+	@Override
+	public String[] getAliases() {
+		return null;
+	}
+
+	@Override
+	public String getInfo() {
+		return "Set the money";
+	}
+
+	@Override
+	public String getPermission() {
+		return "bg.setting.money.first";
+	}
+
+}
