b19ba03f18d82a7008b9a9ce6fd1810f1ff49f85
stefvanschie <stef@van-schie.eu>
2015-09-19 11:33:01 +0200

    Added PM8Click.java

---
+++ me/stefvanschie/buildinggame/events/player/gui/time/PM8Click.java
@@ -0,0 +1,46 @@
+package me.stefvanschie.buildinggame.events.player.gui.time;
+
+import me.stefvanschie.buildinggame.managers.arenas.ArenaManager;
+import me.stefvanschie.buildinggame.utils.Time;
+import me.stefvanschie.buildinggame.utils.plot.Plot;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class PM8Click implements Listener {
+
+	@EventHandler
+	public void onInventoryClick(InventoryClickEvent e) {
+		Player player = (Player) e.getWhoClicked();
+		Inventory inventory = e.getInventory();
+		ItemStack currentItem = e.getCurrentItem();
+		
+		if (ArenaManager.getInstance().getArena(player) == null) {
+			return;
+		}
+		
+		Plot plot = ArenaManager.getInstance().getArena(player).getPlot(player);
+		
+		if (!inventory.getName().equals(ChatColor.GREEN + "Time selection")) {
+			return;
+		}
+		if (currentItem.getType() != Material.WATCH) {
+			return;
+		}
+		if (!currentItem.hasItemMeta()) {
+			return;
+		}
+		if (!currentItem.getItemMeta().getDisplayName().equals(ChatColor.GREEN + "8 PM")) {
+			return;
+		}
+		
+		plot.setTime(Time.PM8);
+		e.setCancelled(true);
+	}
+}
