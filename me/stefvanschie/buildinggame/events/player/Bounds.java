63a082b35518064720e083bd44f4c0d81b9f8cdf
stefvanschie <stef@van-schie.eu>
2015-09-19 10:31:51 +0200

    Update Bounds.java

--- me/stefvanschie/buildinggame/events/player/Bounds.java
+++ me/stefvanschie/buildinggame/events/player/Bounds.java
@@ -4,7 +4,7 @@ import me.stefvanschie.buildinggame.managers.files.SettingsManager;
 import me.stefvanschie.buildinggame.managers.messages.MessageManager;
 import me.stefvanschie.buildinggame.managers.plots.BoundaryManager;
 import me.stefvanschie.buildinggame.utils.Arena;
-import me.stefvanschie.buildinggame.utils.Plot;
+import me.stefvanschie.buildinggame.utils.plot.Plot;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
