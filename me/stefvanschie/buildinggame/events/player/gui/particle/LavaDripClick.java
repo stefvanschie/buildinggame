dd16a95073dc4fdd113c0f61b48c0c11006b2cd8
stefvanschie <stef@van-schie.eu>
2015-09-19 10:42:17 +0200

    Added LavaDripClick.java
    me/stefvanschie/buildinggame/events/player/gui/particle/LavaDripClick.java

---
+++ me/stefvanschie/buildinggame/events/player/gui/particle/LavaDripClick.java
@@ -0,0 +1,48 @@
+package me.stefvanschie.buildinggame.events.player.gui.particle;
+
+import me.stefvanschie.buildinggame.managers.arenas.ArenaManager;
+import me.stefvanschie.buildinggame.utils.particle.Particle;
+import me.stefvanschie.buildinggame.utils.particle.ParticleType;
+import me.stefvanschie.buildinggame.utils.plot.Plot;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class LavaDripClick implements Listener {
+
+	@EventHandler
+	public void onInventoryClick(InventoryClickEvent e) {
+		Player player = (Player) e.getWhoClicked();
+		Inventory inventory = e.getInventory();
+		ItemStack currentItem = e.getCurrentItem();
+		
+		if (ArenaManager.getInstance().getArena(player) == null) {
+			return;
+		}
+		
+		Plot plot = ArenaManager.getInstance().getArena(player).getPlot(player);
+		
+		if (!inventory.getName().equals(ChatColor.GREEN + "Particles")) {
+			return;
+		}
+		if (currentItem.getType() != Material.LAVA_BUCKET) {
+			return;
+		}
+		if (!currentItem.hasItemMeta()) {
+			return;
+		}
+		if (!currentItem.getItemMeta().getDisplayName().equals(ChatColor.GREEN + "Lava drip")) {
+			return;
+		}
+		
+		plot.addParticle(new Particle(player.getLocation(), ParticleType.LAVA_DRIP));
+		e.setCancelled(true);
+	}
+	
+}
