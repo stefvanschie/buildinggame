bcaa76912e496e46a9de783e8bbf466c9191cd11
stefvanschie <stef@van-schie.eu>
2015-09-22 18:10:12 +0200

    Create FloorManager.java

---
+++ me/stefvanschie/buildinggame/managers/plots/FloorManager.java
@@ -0,0 +1,42 @@
+package me.stefvanschie.buildinggame.managers.plots;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import me.stefvanschie.buildinggame.managers.arenas.ArenaManager;
+import me.stefvanschie.buildinggame.managers.files.SettingsManager;
+import me.stefvanschie.buildinggame.utils.Arena;
+import me.stefvanschie.buildinggame.utils.plot.Floor;
+import me.stefvanschie.buildinggame.utils.plot.Plot;
+
+public class FloorManager {
+
+	private FloorManager() {}
+	
+	private static FloorManager instance = new FloorManager();
+	
+	public static FloorManager getInstance() {
+		return instance;
+	}
+	
+	public void setup() {
+		for (Arena arena : ArenaManager.getInstance().getArenas()) {
+			for (Plot plot : arena.getPlots()) {
+				try {
+					YamlConfiguration arenas = SettingsManager.getInstance().getArenas();
+					plot.setFloor(new Floor(Bukkit.getWorld(arenas.getString(arena.getName() + "." + plot.getID() + ".floor.high.world")),
+							arenas.getInt(arena.getName() + "." + plot.getID() + ".floor.high.x"),
+							arenas.getInt(arena.getName() + "." + plot.getID() + ".floor.high.y"),
+							arenas.getInt(arena.getName() + "." + plot.getID() + ".floor.high.z"),
+							arenas.getInt(arena.getName() + "." + plot.getID() + ".floor.low.x"),
+							arenas.getInt(arena.getName() + "." + plot.getID() + ".floor.low.y"),
+							arenas.getInt(arena.getName() + "." + plot.getID() + ".floor.low.z")));
+				} catch (NullPointerException npe) {
+					plot.setFloor(null);
+				} catch (IllegalArgumentException iae) {
+					plot.setFloor(null);
+				}
+			}
+		}
+	}
+}
