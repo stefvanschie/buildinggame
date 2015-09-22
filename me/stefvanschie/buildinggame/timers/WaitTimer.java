226569518ea15aeefe5736b90a4f62e4df2d4191
stefvanschie <stef@van-schie.eu>
2015-09-22 18:12:26 +0200

    Update WaitTimer.java

--- me/stefvanschie/buildinggame/timers/WaitTimer.java
+++ me/stefvanschie/buildinggame/timers/WaitTimer.java
@@ -3,7 +3,7 @@ package me.stefvanschie.buildinggame.timers;
 import me.stefvanschie.buildinggame.managers.files.SettingsManager;
 import me.stefvanschie.buildinggame.managers.messages.MessageManager;
 import me.stefvanschie.buildinggame.utils.Arena;
-import me.stefvanschie.buildinggame.utils.Plot;
+import me.stefvanschie.buildinggame.utils.plot.Plot;
 
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
@@ -12,12 +12,10 @@ import org.bukkit.scheduler.BukkitRunnable;
 public class WaitTimer extends BukkitRunnable {
 
 	private int seconds;
-	private int originalSeconds;
 	private Arena arena;
 	
 	public WaitTimer(int seconds, Arena arena) {
 		this.seconds = seconds;
-		originalSeconds = seconds;
 		this.arena = arena;
 	}
 	
@@ -28,7 +26,7 @@ public class WaitTimer extends BukkitRunnable {
 		if (seconds <= 0) {
 			arena.start();
 			this.cancel();
-		} else if ((seconds != originalSeconds && seconds % 15 == 0) || (seconds <= 10 && seconds >= 1)) {
+		} else if (seconds % 15 == 0 || (seconds <= 10 && seconds >= 1)) {
 			for (Plot plot : arena.getPlots()) {
 				if (plot.getPlayerData() != null) {
 					Player player = plot.getPlayerData().getPlayer();
