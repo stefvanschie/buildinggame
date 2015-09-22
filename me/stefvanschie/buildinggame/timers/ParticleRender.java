9d0579be54a4a32188f8ac9fa9849f4cfcfbd3a5
stefvanschie <stef@van-schie.eu>
2015-09-22 18:11:58 +0200

    Create ParticleRender.java

---
+++ me/stefvanschie/buildinggame/timers/ParticleRender.java
@@ -0,0 +1,22 @@
+package me.stefvanschie.buildinggame.timers;
+
+import me.stefvanschie.buildinggame.managers.arenas.ArenaManager;
+import me.stefvanschie.buildinggame.utils.Arena;
+import me.stefvanschie.buildinggame.utils.particle.Particle;
+import me.stefvanschie.buildinggame.utils.plot.Plot;
+
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class ParticleRender extends BukkitRunnable {
+
+	@Override
+	public void run() {
+		for (Arena arena : ArenaManager.getInstance().getArenas()) {
+			for (Plot plot : arena.getPlots()) {
+				for (Particle particle : plot.getParticles()) {
+					particle.render();
+				}
+			}
+		}
+	}
+}
