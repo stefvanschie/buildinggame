21125396a318f268995eef23eca77511661ad2fb
stefvanschie <stef@van-schie.eu>
2015-09-22 18:07:26 +0200

    Update ArenaManager.java

--- me/stefvanschie/buildinggame/managers/arenas/ArenaManager.java
+++ me/stefvanschie/buildinggame/managers/arenas/ArenaManager.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 import me.stefvanschie.buildinggame.managers.files.SettingsManager;
 import me.stefvanschie.buildinggame.utils.Arena;
-import me.stefvanschie.buildinggame.utils.Plot;
+import me.stefvanschie.buildinggame.utils.plot.Plot;
 
 import org.bukkit.entity.Player;
 
@@ -25,6 +25,7 @@ public class ArenaManager {
 	private List<Arena> arenas = new ArrayList<Arena>();
 	
 	public void setup() {
+		arenas.clear();
 		Set<String> arenas = SettingsManager.getInstance().getArenas().getKeys(false);
 		for (String arena : arenas) {
 			if (!arena.equals("main-spawn"))
