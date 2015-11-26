55cdb617b482c097c31afebd6ff02eeaf51dba3e
stefvanschie <stef@van-schie.eu>
2015-11-26 15:36:11 +0100

    BonemealEvent.java

---
+++ me/stefvanschie/buildinggame/events/player/BonemealEvent.java
@@ -0,0 +1,51 @@
+package me.stefvanschie.buildinggame.events.player;
+
+import me.stefvanschie.buildinggame.managers.arenas.ArenaManager;
+import me.stefvanschie.buildinggame.managers.files.SettingsManager;
+import me.stefvanschie.buildinggame.managers.messages.MessageManager;
+import me.stefvanschie.buildinggame.utils.Arena;
+import me.stefvanschie.buildinggame.utils.plot.Plot;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class BonemealEvent implements Listener {
+
+	@EventHandler
+	public void onPlayerInteract(PlayerInteractEvent e) {
+		YamlConfiguration messages = SettingsManager.getInstance().getMessages();
+		
+		Action action  = e.getAction();
+		Block block = e.getClickedBlock();
+		Player player = e.getPlayer();
+		
+		if (ArenaManager.getInstance().getArena(player) == null) {
+			return;
+		}
+		
+		Arena arena = ArenaManager.getInstance().getArena(player);
+		Plot plot = arena.getPlot(player);
+		
+		if (action != Action.RIGHT_CLICK_BLOCK) {
+			return;
+		}
+		
+		if (block.getType() != Material.GRASS) {
+			return;
+		}
+		
+		if (plot.getBoundary().isInside(block.getLocation())) {
+			return;
+		}
+		
+		MessageManager.getInstance().send(player, messages.getString("in-game.build-out-bounds")
+				.replaceAll("&", "§"));
+		e.setCancelled(true);
+	}
+}
