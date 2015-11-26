04f8e5824980f99180664c7bb15c5d4835123bfd
stefvanschie <stef@van-schie.eu>
2015-11-26 15:00:23 +0100

    BlockPlace.java

--- me/stefvanschie/buildinggame/events/block/BlockPlace.java
+++ me/stefvanschie/buildinggame/events/block/BlockPlace.java
@@ -1,12 +1,14 @@
 package me.stefvanschie.buildinggame.events.block;
 
 import me.stefvanschie.buildinggame.managers.arenas.ArenaManager;
+import me.stefvanschie.buildinggame.managers.files.SettingsManager;
 import me.stefvanschie.buildinggame.managers.messages.MessageManager;
 import me.stefvanschie.buildinggame.utils.Arena;
 import me.stefvanschie.buildinggame.utils.GameState;
 import me.stefvanschie.buildinggame.utils.plot.Plot;
 
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,6 +18,8 @@ public class BlockPlace implements Listener {
 
 	@EventHandler
 	public void onBlockPlace(BlockPlaceEvent e) {
+		YamlConfiguration messages = SettingsManager.getInstance().getMessages();
+		
 		Player player = e.getPlayer();
 		
 		if (ArenaManager.getInstance().getArena(player) == null) {
@@ -26,11 +30,14 @@ public class BlockPlace implements Listener {
 		Plot plot = arena.getPlot(player);
 		
 		if (arena.getState() != GameState.BUILDING) {
+			MessageManager.getInstance().send(player, ChatColor.RED + "You can not build right now");
+			e.setCancelled(true);
 			return;
 		}
 		
 		if (!plot.getBoundary().isInside(e.getBlock().getLocation())) {
-			MessageManager.getInstance().send(player, ChatColor.RED + "You can't build here!");
+			MessageManager.getInstance().send(player, messages.getString("in-game.build-out-bounds")
+					.replaceAll("&", "§"));
 			e.setCancelled(true);
 			return;
 		}
