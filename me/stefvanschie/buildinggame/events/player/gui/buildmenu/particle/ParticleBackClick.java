58b511ba72e7473362d819114a90f7316121e25d
stefvanschie <stef@van-schie.eu>
2015-11-26 15:07:44 +0100

    ParticleBackClick.java

---
+++ me/stefvanschie/buildinggame/events/player/gui/buildmenu/particle/ParticleBackClick.java
@@ -0,0 +1,57 @@
+package me.stefvanschie.buildinggame.events.player.gui.buildmenu.particle;
+
+import me.stefvanschie.buildinggame.managers.arenas.ArenaManager;
+import me.stefvanschie.buildinggame.managers.files.SettingsManager;
+import me.stefvanschie.buildinggame.utils.guis.buildmenu.BuildMenu;
+
+import org.bukkit.Material;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class ParticleBackClick implements Listener {
+
+	@EventHandler
+	public void onInventoryClick(InventoryClickEvent e) {
+		YamlConfiguration messages = SettingsManager.getInstance().getMessages();
+		
+		Player player = (Player) e.getWhoClicked();
+		Inventory inventory = e.getInventory();
+		
+		if (ArenaManager.getInstance().getArena(player) == null) {
+			return;
+		}
+		
+		if (!inventory.getName().equals(messages.getString("gui.particles.title")
+				.replaceAll("&", "§"))) {
+			return;
+		}
+		
+		if (e.getCurrentItem() == null) {
+			return;
+		}
+		
+		ItemStack currentItem = e.getCurrentItem();
+		
+		if (currentItem.getType() != Material.BOOK) {
+			return;
+		}
+		if (!currentItem.hasItemMeta()) {
+			return;
+		}
+		if (!currentItem.getItemMeta().getDisplayName().equals(messages.getString("gui.particles.back.name")
+				.replaceAll("&", "§"))) {
+			return;
+		}
+		
+		BuildMenu menu = new BuildMenu();
+		menu.show(player);
+		
+		e.setCancelled(true);
+	}
+	
+}
