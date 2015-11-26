36781f894b32b3e1f77eec9cf023815a06fa1594
stefvanschie <stef@van-schie.eu>
2015-11-26 15:20:39 +0100

    Speed4Click.java

---
+++ me/stefvanschie/buildinggame/events/player/gui/buildmenu/speed/Speed4Click.java
@@ -0,0 +1,52 @@
+package me.stefvanschie.buildinggame.events.player.gui.buildmenu.speed;
+
+import me.stefvanschie.buildinggame.managers.arenas.ArenaManager;
+import me.stefvanschie.buildinggame.managers.files.SettingsManager;
+
+import org.bukkit.Material;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class Speed4Click implements Listener {
+
+	@EventHandler
+	public void onInventoryClick(InventoryClickEvent e) {
+		YamlConfiguration messages = SettingsManager.getInstance().getMessages();
+		
+		Player player = (Player) e.getWhoClicked();
+		Inventory inventory = e.getInventory();
+		
+		if (ArenaManager.getInstance().getArena(player) == null) {
+			return;
+		}
+		if (!inventory.getName().equals(messages.getString("gui.fly-speed.title")
+				.replaceAll("&", "§"))) {
+			return;
+		}
+		
+		if (e.getCurrentItem() == null) {
+			return;
+		}
+		
+		ItemStack currentItem = e.getCurrentItem();
+		
+		if (currentItem.getType() != Material.FEATHER) {
+			return;
+		}
+		if (!currentItem.hasItemMeta()) {
+			return;
+		}
+		if (!currentItem.getItemMeta().getDisplayName().equals(messages.getString("gui.fly-speed.speed-4.name")
+				.replaceAll("&", "§"))) {
+			return;
+		}
+		
+		player.setFlySpeed((float) 0.4);
+		e.setCancelled(true);
+	}
+}
