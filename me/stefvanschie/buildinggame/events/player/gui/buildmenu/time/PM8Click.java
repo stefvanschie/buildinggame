0e6c53396734b4297bd98630dc6d38a2bb5ee6c4
stefvanschie <stef@van-schie.eu>
2015-11-26 15:25:37 +0100

    PM8Click.java

---
+++ me/stefvanschie/buildinggame/events/player/gui/buildmenu/time/PM8Click.java
@@ -0,0 +1,57 @@
+package me.stefvanschie.buildinggame.events.player.gui.buildmenu.time;
+
+import me.stefvanschie.buildinggame.managers.arenas.ArenaManager;
+import me.stefvanschie.buildinggame.managers.files.SettingsManager;
+import me.stefvanschie.buildinggame.utils.Time;
+import me.stefvanschie.buildinggame.utils.plot.Plot;
+
+import org.bukkit.Material;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class PM8Click implements Listener {
+
+	@EventHandler
+	public void onInventoryClick(InventoryClickEvent e) {
+		YamlConfiguration messages = SettingsManager.getInstance().getMessages();
+		
+		Player player = (Player) e.getWhoClicked();
+		Inventory inventory = e.getInventory();
+		
+		if (ArenaManager.getInstance().getArena(player) == null) {
+			return;
+		}
+		
+		Plot plot = ArenaManager.getInstance().getArena(player).getPlot(player);
+		
+		if (!inventory.getName().equals(messages.getString("gui.time.title")
+				.replaceAll("&", "§"))) {
+			return;
+		}
+		
+		if (e.getCurrentItem() == null) {
+			return;
+		}
+		
+		ItemStack currentItem = e.getCurrentItem();
+		
+		if (currentItem.getType() != Material.WATCH) {
+			return;
+		}
+		if (!currentItem.hasItemMeta()) {
+			return;
+		}
+		if (!currentItem.getItemMeta().getDisplayName().equals(messages.getString("gui.time.8pm.name")
+				.replaceAll("&", "§"))) {
+			return;
+		}
+		
+		plot.setTime(Time.PM8);
+		e.setCancelled(true);
+	}
+}
