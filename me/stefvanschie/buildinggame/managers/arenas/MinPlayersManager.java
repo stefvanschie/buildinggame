01023b389f27086a87f7df3b4a3ec333e26757d1
stefvanschie <stef@van-schie.eu>
2015-11-26 15:41:41 +0100

    MinPlayersManager.java

--- me/stefvanschie/buildinggame/managers/arenas/MinPlayersManager.java
+++ me/stefvanschie/buildinggame/managers/arenas/MinPlayersManager.java
@@ -2,6 +2,7 @@ package me.stefvanschie.buildinggame.managers.arenas;
 
 import org.bukkit.configuration.file.YamlConfiguration;
 
+import me.stefvanschie.buildinggame.Main;
 import me.stefvanschie.buildinggame.managers.files.SettingsManager;
 import me.stefvanschie.buildinggame.utils.Arena;
 
@@ -20,6 +21,9 @@ public class MinPlayersManager {
 			YamlConfiguration arenas = SettingsManager.getInstance().getArenas();
 			try {
 				arena.setMinPlayers(arenas.getInt(arena.getName() + ".minplayers"));
+				if (SettingsManager.getInstance().getConfig().getBoolean("debug")) {
+					Main.getInstance().getLogger().info("Loaded min players for " + arena.getName());
+				}
 			} catch (NullPointerException npe) {
 				arena.setMinPlayers(0);
 			}
