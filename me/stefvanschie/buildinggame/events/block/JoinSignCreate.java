3eba269fcc06fdfd5e69f29b7b9a6522d6f153ba
stefvanschie <stef@van-schie.eu>
2015-11-26 15:00:57 +0100

    JoinSignCreate.java

--- me/stefvanschie/buildinggame/events/block/JoinSignCreate.java
+++ me/stefvanschie/buildinggame/events/block/JoinSignCreate.java
@@ -1,46 +1,58 @@
 package me.stefvanschie.buildinggame.events.block;
 
 import me.stefvanschie.buildinggame.managers.arenas.ArenaManager;
+import me.stefvanschie.buildinggame.managers.arenas.SignManager;
 import me.stefvanschie.buildinggame.managers.files.SettingsManager;
-import me.stefvanschie.buildinggame.managers.messages.MessageManager;
 import me.stefvanschie.buildinggame.utils.Arena;
 
-import org.bukkit.ChatColor;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.block.BlockBreakEvent;
 
-public class JoinSignCreate implements Listener {
+public class JoinSignBreak implements Listener {
 
-	@EventHandler
-	public void onSignChange(SignChangeEvent e) {
-		YamlConfiguration messages = SettingsManager.getInstance().getMessages();
+	@EventHandler(ignoreCancelled = true)
+	public void onBlockBreak(BlockBreakEvent e) {
+		YamlConfiguration signs = SettingsManager.getInstance().getSigns();
 		
-		Player player = e.getPlayer();
+		Block block = e.getBlock();
 		
-		if (!e.getLine(0).equalsIgnoreCase("[buildinggame]")) {
-			return;
-		}
-		if (!e.getLine(1).equalsIgnoreCase("join")) {
-			return;
-		}
-		if (ArenaManager.getInstance().getArena(e.getLine(2)) == null) {
-			MessageManager.getInstance().send(player, ChatColor.RED + "That's not a valid arena");
+		if (!(block.getState() instanceof Sign)) {
 			return;
 		}
 		
-		Arena arena = ArenaManager.getInstance().getArena(e.getLine(2));
+		Sign sign = (Sign) block.getState();
 		
-		if (!player.hasPermission("bg.sign.create")) {
-			MessageManager.getInstance().send(player, messages.getString("global.permissionNode"));
-			return;
+		for (Arena arena : ArenaManager.getInstance().getArenas()) {
+			if (!arena.getSigns().contains(sign)) {
+				continue;
+			}
+			
+			for (String string : signs.getKeys(false)) {
+				if (signs.getString(string + ".arena") != arena.getName()) {
+					continue;
+				}
+				if (signs.getString(string + ".world") != sign.getLocation().getWorld().getName()) {
+					continue;
+				}
+				if (signs.getInt(string + ".x") != sign.getLocation().getBlockX()) {
+					continue;
+				}
+				if (signs.getInt(string + ".y") != sign.getLocation().getBlockY()) {
+					continue;
+				}
+				if (signs.getInt(string + ".z") != sign.getLocation().getBlockZ()) {
+					continue;
+				}
+				SettingsManager.getInstance().save();
+				
+				SignManager.getInstance().setup();
+				
+				signs.set(string, null);
+			}
 		}
-		
-		e.setLine(0, ChatColor.BOLD + "BuildingGame");
-		e.setLine(1, ChatColor.DARK_GRAY + "" + ChatColor.ITALIC + "join");
-		e.setLine(2, ChatColor.UNDERLINE + "Map: " + arena.getName());
-		e.setLine(3, arena.getPlayers() + "/" +arena.getMaxPlayers());
 	}
 }
