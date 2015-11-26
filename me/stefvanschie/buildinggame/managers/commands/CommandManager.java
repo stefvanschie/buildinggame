6eb559e8c0ec2631e91c64033ada672cbb77290f
stefvanschie <stef@van-schie.eu>
2015-11-26 15:42:29 +0100

    CommandManager.java

--- me/stefvanschie/buildinggame/managers/commands/CommandManager.java
+++ me/stefvanschie/buildinggame/managers/commands/CommandManager.java
@@ -7,8 +7,11 @@ import java.util.List;
 import me.stefvanschie.buildinggame.commands.commandutils.CommandResult;
 import me.stefvanschie.buildinggame.commands.commandutils.SubCommand;
 import me.stefvanschie.buildinggame.commands.subcommands.CreateArena;
+import me.stefvanschie.buildinggame.commands.subcommands.DeleteArena;
+import me.stefvanschie.buildinggame.commands.subcommands.DeleteSpawn;
+import me.stefvanschie.buildinggame.commands.subcommands.ForceStart;
 import me.stefvanschie.buildinggame.commands.subcommands.Join;
-import me.stefvanschie.buildinggame.commands.subcommands.Leave;
+import me.stefvanschie.buildinggame.commands.subcommands.LeaveCommand;
 import me.stefvanschie.buildinggame.commands.subcommands.Reload;
 import me.stefvanschie.buildinggame.commands.subcommands.SetBounds;
 import me.stefvanschie.buildinggame.commands.subcommands.SetFloor;
@@ -18,12 +21,14 @@ import me.stefvanschie.buildinggame.commands.subcommands.SetMinPlayers;
 import me.stefvanschie.buildinggame.commands.subcommands.SetSpawn;
 import me.stefvanschie.buildinggame.commands.subcommands.Setting;
 import me.stefvanschie.buildinggame.commands.subcommands.VoteCommand;
+import me.stefvanschie.buildinggame.managers.files.SettingsManager;
 import me.stefvanschie.buildinggame.managers.messages.MessageManager;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
 public class CommandManager implements CommandExecutor {
@@ -33,8 +38,11 @@ public class CommandManager implements CommandExecutor {
 	public void setup() {
 		subCommands.clear();
 		subCommands.add(new CreateArena());
+		subCommands.add(new DeleteArena());
+		subCommands.add(new DeleteSpawn());
+		subCommands.add(new ForceStart());
 		subCommands.add(new Join());
-		subCommands.add(new Leave());
+		subCommands.add(new LeaveCommand());
 		subCommands.add(new Reload());
 		subCommands.add(new SetBounds());
 		subCommands.add(new SetFloor());
@@ -48,18 +56,21 @@ public class CommandManager implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		YamlConfiguration messages = SettingsManager.getInstance().getMessages();
 		
 		if (!(sender instanceof Player)) {
-			MessageManager.getInstance().send(sender, "&cOnly players can perform this command");
+			MessageManager.getInstance().send(sender, ChatColor.RED + "Only players can perform this command");
 			return false;
 		}
 		
 		Player player = (Player) sender;
 		
-		if (cmd.getName().equalsIgnoreCase("bg")) {
+		if (cmd.getName().equalsIgnoreCase("bg") || cmd.getName().equalsIgnoreCase("buildinggame")) {
 			if (args.length == 0) {
 				for (SubCommand sc : subCommands) {
-					MessageManager.getInstance().sendWithoutPrefix(player, ChatColor.GREEN + "/bg " + sc.getName() + " - " + sc.getInfo());
+					if (player.hasPermission(sc.getPermission())) {
+						MessageManager.getInstance().sendWithoutPrefix(player, ChatColor.GREEN + "/bg " + sc.getName() + " - " + sc.getInfo());
+					}
 				}
 				return false;
 			}
@@ -68,6 +79,11 @@ public class CommandManager implements CommandExecutor {
 			
 			if (target == null) {
 				MessageManager.getInstance().send(player, ChatColor.RED + args[0] + " is not valid.");
+				return false;
+			}
+			
+			if (!player.hasPermission(target.getPermission())) {
+				MessageManager.getInstance().send(player, messages.getString("global.permissionNode"));
 				return false;
 			}
 			
