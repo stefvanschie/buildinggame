98d29d79e9257e423fbe008ff3d6d8f5968309ba
stefvanschie <stef@van-schie.eu>
2015-11-26 15:45:21 +0100

    PlotManager.java

--- me/stefvanschie/buildinggame/managers/plots/PlotManager.java
+++ me/stefvanschie/buildinggame/managers/plots/PlotManager.java
@@ -1,5 +1,6 @@
 package me.stefvanschie.buildinggame.managers.plots;
 
+import me.stefvanschie.buildinggame.Main;
 import me.stefvanschie.buildinggame.managers.arenas.ArenaManager;
 import me.stefvanschie.buildinggame.managers.files.SettingsManager;
 import me.stefvanschie.buildinggame.utils.Arena;
@@ -17,15 +18,20 @@ public class PlotManager {
 	
 	public void setup() {
 		for (Arena arena : ArenaManager.getInstance().getArenas()) {
+			arena.getPlots().clear();
 			for (String plot : SettingsManager.getInstance().getArenas().getConfigurationSection(arena.getName()).getKeys(false)) {
 				try {
 					Integer.parseInt(plot);
 				} catch (NumberFormatException e) {
 					continue;
 				}
+				
 				Plot p = new Plot(Integer.parseInt(plot));
 				arena.addPlot(p);
 				p.setArena(arena);
+				if (SettingsManager.getInstance().getConfig().getBoolean("debug")) {
+					Main.getInstance().getLogger().info("Loaded plot " + p.getID() + " in arena " + arena.getName());
+				}
 			}
 		}
 	}
