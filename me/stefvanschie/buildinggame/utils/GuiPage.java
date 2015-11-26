d3c2f259d43dbaa89d9dd4ea47fe95c4fb88a07f
stefvanschie <stef@van-schie.eu>
2015-11-26 15:54:59 +0100

    GuiPage.java

---
+++ me/stefvanschie/buildinggame/utils/GuiPage.java
@@ -0,0 +1,18 @@
+package me.stefvanschie.buildinggame.utils;
+
+public class GuiPage {
+
+	private int page;
+	
+	public GuiPage(int page) {
+		this.page = page;
+	}
+	
+	public int getPage() {
+		return page;
+	}
+	
+	public void setPage(int page) {
+		this.page = page;
+	}
+}
