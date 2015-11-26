9088717f0086ba1049c0a6a57014e95e2e5aad7d
stefvanschie <stef@van-schie.eu>
2015-11-26 15:34:37 +0100

    Interact.java

---
+++ me/stefvanschie/buildinggame/events/player/voting/Interact.java
@@ -0,0 +1,29 @@
+package me.stefvanschie.buildinggame.events.player.voting;
+
+import me.stefvanschie.buildinggame.managers.arenas.ArenaManager;
+import me.stefvanschie.buildinggame.utils.Arena;
+import me.stefvanschie.buildinggame.utils.GameState;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class Interact implements Listener {
+
+	@EventHandler
+	public void onPlayerInteract(PlayerInteractEvent e) {
+		Player player = e.getPlayer();
+		
+		if (ArenaManager.getInstance().getArena(player) == null) {
+			return;
+		}
+		
+		Arena arena = ArenaManager.getInstance().getArena(player);
+		
+		if (arena.getState() == GameState.VOTING) {
+			e.setCancelled(true);
+			return;
+		}
+	}
+}
