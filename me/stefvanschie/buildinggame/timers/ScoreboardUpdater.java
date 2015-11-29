a5fdca9dee73f175190364316bdefc0fb85b9fcb
stefvanschie <stef@van-schie.eu>
2015-11-29 20:05:53 +0100

    Update ScoreboardUpdater.java

--- me/stefvanschie/buildinggame/timers/ScoreboardUpdater.java
+++ me/stefvanschie/buildinggame/timers/ScoreboardUpdater.java
@@ -2,8 +2,9 @@ package me.stefvanschie.buildinggame.timers;
 
 import me.stefvanschie.buildinggame.managers.arenas.ArenaManager;
 import me.stefvanschie.buildinggame.managers.files.SettingsManager;
-import me.stefvanschie.buildinggame.utils.Arena;
+import me.stefvanschie.buildinggame.utils.GamePlayer;
 import me.stefvanschie.buildinggame.utils.GameState;
+import me.stefvanschie.buildinggame.utils.arena.Arena;
 import me.stefvanschie.buildinggame.utils.plot.Plot;
 
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -18,20 +19,26 @@ public class ScoreboardUpdater extends BukkitRunnable {
 		for (Arena arena : ArenaManager.getInstance().getArenas()) {
 			if (arena.getState() == GameState.VOTING && !config.getBoolean("names-after-voting")) {
 				for (Plot plot : arena.getUsedPlots()) {
-					arena.getScoreboard().show(plot.getGamePlayer().getPlayer());
+					for (GamePlayer gamePlayer : plot.getGamePlayers()) {
+						arena.getScoreboard().show(gamePlayer.getPlayer());
+					}
 				}
 				return;
 			}
 			
 			if (arena.getState() == GameState.RESETING && config.getBoolean("names-after-voting")) {
 				for (Plot plot : arena.getUsedPlots()) {
-					arena.getScoreboard().show(plot.getGamePlayer().getPlayer());
+					for (GamePlayer gamePlayer : plot.getGamePlayers()) {
+						arena.getScoreboard().show(gamePlayer.getPlayer());
+					}
 				}
 				return;
 			}
 			
 			for (Plot plot : arena.getUsedPlots()) {
-				arena.getBuildScoreboard().update(plot.getGamePlayer().getPlayer());
+				for (GamePlayer gamePlayer : plot.getGamePlayers()) {
+					arena.getBuildScoreboard().update(gamePlayer.getPlayer());
+				}
 			}
 		}
 	}
