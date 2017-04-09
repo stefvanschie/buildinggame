e4c86e76a86685884bbb9c17ecf868111ac1ddd1
stefvanschie <stefvanschiedev@gmail.com>
2017-04-09 20:45:44 +0200

    Armorstands cannot be spawned outside a plot & no ai is fixed again
    Now armorstands cannot be placed outside plots when playing a game
    anymore and placing one (or any entity) isnisde your plot won't throw an
    error anymore.

--- buildinggame/src/com/gmail/stefvanschiedev/buildinggame/events/entity/EntitySpawn.java
+++ buildinggame/src/com/gmail/stefvanschiedev/buildinggame/events/entity/EntitySpawn.java
@@ -1,12 +1,16 @@
 package com.gmail.stefvanschiedev.buildinggame.events.entity;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Vehicle;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.hanging.HangingPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.vehicle.VehicleCreateEvent;
 
 import com.gmail.stefvanschiedev.buildinggame.managers.arenas.ArenaManager;
@@ -50,7 +54,43 @@ public class EntitySpawn implements Listener {
 		} else
 			e.setCancelled(true);
 	}
-	
+
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerInteract(PlayerInteractEvent e) {
+        Player player = e.getPlayer();
+
+        if (ArenaManager.getInstance().getArena(player) == null || e.getItem() == null || (e.getItem().getType() != Material.MONSTER_EGG && e.getItem().getType() != Material.ARMOR_STAND) || e.getAction() != Action.RIGHT_CLICK_BLOCK)
+            return;
+
+        Location improvedLocation = e.getClickedBlock().getLocation();
+
+        switch (e.getBlockFace()) {
+            case UP:
+                improvedLocation.add(0, 1, 0);
+                break;
+            case DOWN:
+                improvedLocation.subtract(0, 1, 0);
+                break;
+            case NORTH:
+                improvedLocation.subtract(0, 0, 1);
+                break;
+            case EAST:
+                improvedLocation.add(1, 0, 0);
+                break;
+            case SOUTH:
+                improvedLocation.add(0, 0, 1);
+                break;
+            case WEST:
+                improvedLocation.subtract(1, 0, 0);
+                break;
+            default:
+                break;
+        }
+
+        if (!ArenaManager.getInstance().getArena(player).getPlot(player).getBoundary().isInside(improvedLocation))
+            e.setCancelled(true);
+    }
+
 	private Plot isInside(Location location) {
 		for (Arena arena : ArenaManager.getInstance().getArenas()) {
 			for (Plot plot : arena.getPlots()) {
--- buildinggame/src/com/gmail/stefvanschiedev/buildinggame/utils/nbt/entity/NbtFactory.java
+++ buildinggame/src/com/gmail/stefvanschiedev/buildinggame/utils/nbt/entity/NbtFactory.java
@@ -24,7 +24,18 @@ public class NbtFactory {
     private static final BiMap<Integer, Class<?>> NBT_CLASS = HashBiMap.create();
     private static final BiMap<Integer, NbtType> NBT_ENUM = HashBiMap.create();
     
+    @SuppressWarnings("unused")
     private enum NbtType {
+        TAG_END(0, Void.class),
+        TAG_BYTE(1, byte.class),
+        TAG_SHORT(2, short.class),
+        TAG_INT(3, int.class),
+        TAG_LONG(4, long.class),
+        TAG_FLOAT(5, float.class),
+        TAG_DOUBLE(6, double.class),
+        TAG_BYTE_ARRAY(7, byte[].class),
+        TAG_INT_ARRAY(11, int[].class),
+        TAG_STRING(8, String.class),
         TAG_LIST(9, List.class),
         TAG_COMPOUND(10, Map.class);
 
