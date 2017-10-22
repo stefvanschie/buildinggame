5b51581b91d702ea0274b5e981908122b4493a1f
stefvanschie <stefvanschiedev@gmail.com>
2017-10-22 17:51:59 +0200

    Fix error when player quits
    When a player left the server without having at least one on every
    statistic, an error would occur. This has now been fixed.

--- buildinggame/src/com/gmail/stefvanschiedev/buildinggame/events/stats/database/QuitPlayerStats.java
+++ buildinggame/src/com/gmail/stefvanschiedev/buildinggame/events/stats/database/QuitPlayerStats.java
@@ -1,5 +1,6 @@
 package com.gmail.stefvanschiedev.buildinggame.events.stats.database;
 
+import com.gmail.stefvanschiedev.buildinggame.utils.stats.Stat;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -31,15 +32,18 @@ public class QuitPlayerStats implements Listener {
 	@EventHandler
     public static void onPlayerQuit(PlayerQuitEvent event){
         final Player player = event.getPlayer();
+
         new BukkitRunnable() {
             @Override
             public void run() {
                 StatManager instance = StatManager.getInstance();
 
-                for (StatType type : StatType.values())
+                for (StatType type : StatType.values()) {
+                    Stat stat = instance.getStat(player, type);
+
                     instance.getMySQLDatabase().setStat(player.getUniqueId().toString(),
-                            type.toString().toLowerCase(Locale.getDefault()),
-                            instance.getStat(player, type).getValue());
+                            type.toString().toLowerCase(Locale.getDefault()), stat == null ? 0 : stat.getValue());
+                }
             }
         }.runTaskAsynchronously(Main.getInstance());
 	}
