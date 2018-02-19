46844ca537fc909b60d27df22c5969fba80cdf33
stefvanschie <stefvanschiedev@gmail.com>
2018-02-19 20:06:16 +0100

    Add %buildinggame_stat_<stat>_top_<number>%
    Addresses issue #115.

--- buildinggame/src/main/java/com/gmail/stefvanschiedev/buildinggame/managers/softdependencies/PlaceholderAPIPlaceholders.java
+++ buildinggame/src/main/java/com/gmail/stefvanschiedev/buildinggame/managers/softdependencies/PlaceholderAPIPlaceholders.java
@@ -9,11 +9,13 @@ import com.gmail.stefvanschiedev.buildinggame.utils.arena.Arena;
 import com.gmail.stefvanschiedev.buildinggame.utils.stats.Stat;
 import com.gmail.stefvanschiedev.buildinggame.utils.stats.StatType;
 import me.clip.placeholderapi.external.EZPlaceholderHook;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.Collection;
-import java.util.Locale;
+import java.util.*;
 
 /**
  * Registers placeholder for PlaceholderAPI
@@ -42,53 +44,180 @@ public class PlaceholderAPIPlaceholders extends EZPlaceholderHook {
     @Override
     public String onPlaceholderRequest(Player player, String identifier) {
         if (identifier.equalsIgnoreCase("players"))
-            return String.valueOf(ArenaManager.getInstance().getArenas().stream().mapToInt(Arena::getPlayers).sum());
+            return players();
+
+        if (identifier.toLowerCase(Locale.getDefault()).startsWith("stat_")) {
+            for (StatType statType : StatType.values()) {
+                String type = statType.toString().toLowerCase(Locale.getDefault());
+
+                if (!identifier.startsWith("stat_" + type + "_top_"))
+                    continue;
+
+                int number;
+
+                try {
+                    number = Integer.parseInt(identifier.substring(("stat_" + type + "_top_").length()));
+                } catch (NumberFormatException ignore) {
+                    return null;
+                }
+
+                return statTop(statType, number);
+            }
+
+            return null;
+        }
 
         //divider between non-player dependent placeholders and player dependent placeholders
         if (player == null)
             return null;
 
-        if (identifier.toLowerCase(Locale.getDefault()).startsWith("stat_" )) {
+        if (identifier.toLowerCase(Locale.getDefault()).startsWith("stat_")) {
             for (StatType statType : StatType.values()) {
                 if (!identifier.equalsIgnoreCase("stat_" + statType.toString().toLowerCase(Locale
                     .getDefault())))
                     continue;
 
-                Stat stat = StatManager.getInstance().getStat(player, statType);
-
-                return stat == null ? "0" : String.valueOf(stat.getValue());
+                return stat(player, statType);
             }
 
             return null;
         }
 
         if (identifier.equalsIgnoreCase("has_booster"))
-            return Booster.hasBooster(player) ? messages.getString("placeholder-api.has-booster.result.true") :
-                messages.getString("placeholder-api.has-booster.result.false");
+            return hasBooster(player);
 
         if (identifier.equalsIgnoreCase("booster_multiplier"))
-            return String.valueOf(Booster.getMultiplier(player));
+            return boosterMultiplier(player);
 
-        Collection<Booster> boosters = Booster.getBoosters(player);
         if (identifier.equalsIgnoreCase("booster_time_left"))
-            return String.valueOf(boosters.stream().mapToInt(Booster::getRemainingTime).max()
-                .orElse(0));
+            return boosterTimeLeft(player);
 
-        if (identifier.equalsIgnoreCase("booster_activator")) {
-            if (boosters.isEmpty())
-                return "";
-
-            StringBuilder activators = new StringBuilder();
-            boosters.stream().map(booster -> booster.getActivator().getName()).distinct().forEach(name -> activators
-                .append(name).append(", "));
-
-            if (boosters.stream().map(booster -> booster.getActivator().getName()).distinct().count() == 1)
-                return activators.substring(0, activators.length() - 2);
-
-            return activators.replace(activators.length() - 2, activators.length(), "").replace(activators
-                .lastIndexOf(", "), activators.lastIndexOf(", ") + 2, " and ").toString();
-        }
+        if (identifier.equalsIgnoreCase("booster_activator"))
+            return boosterActivator(player);
 
         return null;
+    }
+
+    /**
+     * Evaluates %buildinggame_players% placeholders
+     *
+     * @return the amount of players
+     * @since 5.5.4
+     */
+    @NotNull
+    @Contract(pure = true)
+    private String players() {
+        return String.valueOf(ArenaManager.getInstance().getArenas().stream().mapToInt(Arena::getPlayers).sum());
+    }
+
+    /**
+     * Evaluates %buildinggame_stat_<stat>% placeholders
+     *
+     * @param player the player to evaluate this placeholder for
+     * @param statType the statistic type
+     * @return the value for the specified stat
+     * @since 5.5.4
+     */
+    @NotNull
+    @Contract(pure = true)
+    private String stat(OfflinePlayer player, StatType statType) {
+        Stat stat = StatManager.getInstance().getStat(player, statType);
+
+        return stat == null ? "0" : String.valueOf(stat.getValue());
+    }
+
+    /**
+     * Evaluates %buildinggame_has_booster% placeholders
+     *
+     * @param player the player to evaluate this placeholder for
+     * @return the translated string for true/false
+     * @since 5.5.4
+     */
+    @NotNull
+    @Contract(pure = true)
+    private String hasBooster(Player player) {
+        return Booster.hasBooster(player) ? messages.getString("placeholder-api.has-booster.result.true") :
+            messages.getString("placeholder-api.has-booster.result.false");
+    }
+
+    /**
+     * Evaluates %buildinggame_booster_multiplier% placeholders
+     *
+     * @param player the player to evaluate this placeholder for
+     * @return the total multiplier
+     * @since 5.5.4
+     */
+    @NotNull
+    @Contract(pure = true)
+    private String boosterMultiplier(Player player) {
+        return String.valueOf(Booster.getMultiplier(player));
+    }
+
+    /**
+     * Evaluates %buildinggame_booster_time_left% placeholders
+     *
+     * @param player the player to evaluate this placeholder for
+     * @return the time remaining on the currently longest during booster
+     * @since 5.5.4
+     */
+    @NotNull
+    @Contract(pure = true)
+    private String boosterTimeLeft(Player player) {
+        return String.valueOf(Booster.getBoosters(player).stream().mapToInt(Booster::getRemainingTime).max()
+            .orElse(0));
+    }
+
+    /**
+     * Evaluates %buildinggame_booster_activator% placeholders
+     *
+     * @param player the player to evaluate this placeholder for
+     * @return the activators of all boosters for the player
+     * @since 5.5.4
+     */
+    @NotNull
+    @Contract(pure = true)
+    private String boosterActivator(Player player) {
+        Collection<Booster> boosters = Booster.getBoosters(player);
+
+        if (boosters.isEmpty())
+            return "";
+
+        StringBuilder activators = new StringBuilder();
+        boosters.stream().map(booster -> booster.getActivator().getName()).distinct().forEach(name -> activators
+            .append(name).append(", "));
+
+        if (boosters.stream().map(booster -> booster.getActivator().getName()).distinct().count() == 1)
+            return activators.substring(0, activators.length() - 2);
+
+        return activators.replace(activators.length() - 2, activators.length(), "").replace(activators
+            .lastIndexOf(", "), activators.lastIndexOf(", ") + 2, " and ").toString();
+    }
+
+    /**
+     * Returns the value for the given stat in the specified spot given a descending based ordering
+     *
+     * @param type the stat type
+     * @param number the index
+     * @return the stat value
+     * @since 5.5.4
+     */
+    @NotNull
+    @Contract(pure = true)
+    private String statTop(StatType type, int number) {
+        Map<OfflinePlayer, Integer> stats = new HashMap<>();
+
+        for (Stat stat : StatManager.getInstance().getStats(type))
+            stats.put(stat.getPlayer(), stat.getValue());
+
+        List<Integer> values = new ArrayList<>(stats.values());
+        Collections.sort(values);
+        Collections.reverse(values);
+
+        int value = -1;
+
+        if (values.size() >= number)
+            value = values.get(number - 1);
+
+        return String.valueOf(value);
     }
 }
