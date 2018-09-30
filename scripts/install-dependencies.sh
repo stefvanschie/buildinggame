90f2d7e73418638a85325773b24036382310a0b7
stefvanschie <stefvanschiedev@gmail.com>
2018-09-30 14:41:08 +0200

    Add Travis CI
    Don't know if it works correctly, but I guess we'll see that whenever a
    new push is made.

---
+++ scripts/install-dependencies.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Downloading dependencies with no maven repository available"
+
+wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar -O BuildTools.jar && java -jar BuildTools.jar --rev 1.13.1
+
+wget https://www.robindebaets.be/LeaderHeadsAPI.jar && mvn install:install-file -Dfile=LeaderHeadsAPI.jar -DgroupId=me.robin -DartifactId=leaderheads -Dversion=3.1.6
+
+git clone https://github.com/Gnat008/perworldinventory-kt.git && cd perworldinventory-kt && mvn clean install
+
+git clone https://github.com/I-Al-Istannen/MiniNBT.git && cd MiniNBT && mvn clean install
