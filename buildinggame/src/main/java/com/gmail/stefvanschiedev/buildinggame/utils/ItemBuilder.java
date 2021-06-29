9de07caac43a6eb8fefc16f72b67a3ce86907a66
stefvanschie <stefvanschiedev@gmail.com>
2021-06-29 20:14:24 +0200

    Fix item clicks not working when targeting air

--- buildinggame/src/main/java/com/gmail/stefvanschiedev/buildinggame/utils/ItemBuilder.java
+++ buildinggame/src/main/java/com/gmail/stefvanschiedev/buildinggame/utils/ItemBuilder.java
@@ -193,7 +193,7 @@ public class ItemBuilder {
          * @since 7.0.0
          */
         @Contract("null -> fail")
-        @EventHandler(ignoreCancelled = true)
+        @EventHandler
         private void onPlayerInteract(PlayerInteractEvent e) {
             Player player = e.getPlayer();
 
